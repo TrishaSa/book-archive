eb816d81bc036be2093c777238dfbafc58cfc3e7
Trisha Saha <trishasaha0777@gmail.com>
2021-09-02 19:01:53 +0600

    book store2

--- main.js
+++ main.js
@@ -47,7 +47,7 @@ const displayBook = books => {
     	<div class="card h-100">
       <img src="https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg" class="card-img-top img-fluid h-75">
       <div class="card-body">
-        <h5 class="card-title"> title: ${book.title}</h5>
+        <h5 class="card-title"> title:${book.title ? book.title : ""}</h5>
         <p class="card-text">Author Name: ${book.author_name}</p>
         <p class="card-text">First Publish Year: ${book.first_publish_year}</p>
         <p class="card-text">Publisher: ${book.publisher}</p>
